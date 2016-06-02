91ef1b8f2f38dec0adf2f3de8b523399909a558c
tomohide-takao <tomohide-takao@navitime.co.jp>
2016-06-02 18:52:18 +0900

    wip

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -18,10 +17,6 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
 
 	router.Run(":" + port)
 }
